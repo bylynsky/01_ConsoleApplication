5c629f1c90b42603b5deb4427513ee12cae22bd3
bylynsky <wojtek.bilinski@gmail.com>
2017-10-26 07:45:23 +0200

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp: Definiuje punkt wejścia dla aplikacji konsolowej.
+//
+
+#include "stdafx.h"
+#include <iostream> 
+
+int main()
+{
+	std::cout << "Hello world!\n";
+    return 0;
+}
+
